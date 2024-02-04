af33bb63b7927b73120729976e80246dbd3da726
Vladislav Zainullin <mittoface02@mail.ru>
2024-02-04 17:02:46 +0300

    Выносит инициализацию расширения в static поле

--- EFCore.TemporaryTables/Extensions/DbContextOptionsBuilderExtensions.cs
+++ EFCore.TemporaryTables/Extensions/DbContextOptionsBuilderExtensions.cs
@@ -5,13 +5,15 @@ namespace EFCore.TemporaryTables.Extensions;
 
 public static class DbContextOptionsBuilderExtensions
 {
+    private static readonly TemporaryTableOptionsExtension TemporaryTableOptionsExtension = new();
+
     public static DbContextOptionsBuilder UseTemporaryTables(this DbContextOptionsBuilder optionsBuilder)
     {
         var extension = optionsBuilder.Options.FindExtension<TemporaryTableOptionsExtension>();
         if (!ReferenceEquals(extension, default)) return optionsBuilder;
         
         var infrastructure = optionsBuilder as IDbContextOptionsBuilderInfrastructure;
-        infrastructure.AddOrUpdateExtension(new TemporaryTableOptionsExtension());
+        infrastructure.AddOrUpdateExtension(TemporaryTableOptionsExtension);
 
         return optionsBuilder;
     }
