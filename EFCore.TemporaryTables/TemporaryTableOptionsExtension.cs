978866d87d7a0b45d7c461d7bc2485296fa1f5ec
Vladislav Zainullin <mittoface02@mail.ru>
2024-02-04 16:59:10 +0300

    Добавляет инфраструктуру для временных таблиц

---
+++ EFCore.TemporaryTables/TemporaryTableOptionsExtension.cs
@@ -0,0 +1,44 @@
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EFCore.TemporaryTables;
+
+internal sealed class TemporaryTableOptionsExtension : IDbContextOptionsExtension
+{
+    private ExtensionInfo? _info;
+    
+    public void ApplyServices(IServiceCollection services)
+    {
+    }
+
+    public void Validate(IDbContextOptions options) {}
+
+    public DbContextOptionsExtensionInfo Info => _info ??= new ExtensionInfo(this);
+
+    private sealed class ExtensionInfo : DbContextOptionsExtensionInfo
+    {
+        private readonly Lazy<string> _logFragment = new("using temporary tables");
+
+        public ExtensionInfo(IDbContextOptionsExtension extension) : base(extension)
+        {
+        }
+
+        public override int GetServiceProviderHashCode()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ShouldUseSameServiceProvider(DbContextOptionsExtensionInfo other)
+        {
+            return other is ExtensionInfo;
+        }
+
+        public override void PopulateDebugInfo(IDictionary<string, string> debugInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsDatabaseProvider => default;
+        public override string LogFragment => _logFragment.Value;
+    }
+}
