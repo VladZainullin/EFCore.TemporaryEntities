764938a38e8af837bf52d1c10771bebbcacc1f54
Vladislav Zainullin <mittoface02@mail.ru>
2024-02-05 22:17:26 +0300

    Убирает инициализацию из static поля

--- EFCore.TemporaryTables/Extensions/DbContextOptionsBuilderExtensions.cs
+++ EFCore.TemporaryTables/Extensions/DbContextOptionsBuilderExtensions.cs
@@ -5,15 +5,13 @@ namespace EFCore.TemporaryTables.Extensions;
 
 public static class DbContextOptionsBuilderExtensions
 {
-    private static readonly TemporaryTableOptionsExtension TemporaryTableOptionsExtension = new();
-
     public static DbContextOptionsBuilder UseTemporaryTables(this DbContextOptionsBuilder optionsBuilder)
     {
         var extension = optionsBuilder.Options.FindExtension<TemporaryTableOptionsExtension>();
         if (!ReferenceEquals(extension, default)) return optionsBuilder;
 
         var infrastructure = optionsBuilder as IDbContextOptionsBuilderInfrastructure;
-        infrastructure.AddOrUpdateExtension(TemporaryTableOptionsExtension);
+        infrastructure.AddOrUpdateExtension(new TemporaryTableOptionsExtension());
 
         return optionsBuilder;
     }
