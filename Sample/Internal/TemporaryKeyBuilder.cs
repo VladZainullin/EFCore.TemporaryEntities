4ec75aa0d733065397c681e4285b85899b910f78
Vladislav Zainullin <mittoface02@mail.ru>
2024-02-12 23:26:05 +0300

    Добавляет декоратор key builder

---
+++ Sample/Internal/TemporaryKeyBuilder.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Sample.Internal;
+
+public sealed class TemporaryKeyBuilder : KeyBuilder
+{
+    private readonly KeyBuilder _keyBuilder;
+#pragma warning disable EF1001
+    public TemporaryKeyBuilder(KeyBuilder keyBuilder) : 
+        base(keyBuilder.Metadata)
+#pragma warning restore EF1001
+    {
+        _keyBuilder = keyBuilder;
+    }
+
+    public override IMutableKey Metadata => _keyBuilder.Metadata;
+
+    public override bool Equals(object? obj)
+    {
+        return _keyBuilder.Equals(obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return _keyBuilder.GetHashCode();
+    }
+
+    public override KeyBuilder HasAnnotation(string annotation, object? value)
+    {
+        return _keyBuilder.HasAnnotation(annotation, value);
+    }
+
+    public override string? ToString()
+    {
+        return _keyBuilder.ToString();
+    }
+}
