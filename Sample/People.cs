f823990605fd5fd6b673af29ee07630cdbe81b62
VladislavZainullin <mittoface02@mail.ru>
2024-02-13 12:26:57 +0300

    Переводит проект на dotnet 6

--- Sample/People.cs
+++ Sample/People.cs
@@ -2,51 +2,51 @@ namespace Sample;
 
 public sealed class People
 {
-    public required Guid Id { get; init; }
+    public Guid Id { get; init; }
 
-    public required Identification Identification { get; init; }
+    public Identification Identification { get; init; } = default!;
+    
+    public Family Family { get; init; } = default!;
 
-    public required Family Family { get; init; }
+    public Work Work { get; init; } = default!;
 
-    public required Work Work { get; init; }
-
-    public required Address Address { get; init; }
+    public Address Address { get; init; } = default!;
 }
 
 public sealed class Identification
 {
-    public required string Name { get; init; }
+    public string Name { get; init; } = default!;
 
-    public required string Surname { get; init; }
+    public string Surname { get; init; } = default!;
 
-    public required Gender Gender { get; init; }
+    public Gender Gender { get; init; }
 }
 
 public sealed class Family
 {
-    public required int QuantityOfChildren { get; init; }
+    public int QuantityOfChildren { get; init; }
 
-    public required bool HasPartner { get; init; }
+    public bool HasPartner { get; init; }
 
     public bool HasChildren => QuantityOfChildren != default;
 }
 
 public sealed class Work
 {
-    public required decimal Salary { get; init; }
+    public decimal Salary { get; init; }
 
-    public required Address Address { get; init; }
+    public Address Address { get; init; } = default!;
 }
 
 public sealed class Address
 {
-    public required string Country { get; init; }
+    public string Country { get; init; } = default!;
 
-    public required string Region { get; init; }
+    public string Region { get; init; } = default!;
 
-    public required string City { get; init; }
+    public string City { get; init; } = default!;
 
-    public required string Street { get; init; }
+    public string Street { get; init; } = default!;
 
-    public required string House { get; init; }
+    public string House { get; init; } = default!;
 }
