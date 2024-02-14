06cad8d72aa7e07a2d0db8bd81348c396483cbc2
Vladislav Zainullin <mittoface02@mail.ru>
2024-02-15 00:20:32 +0300

    Revert "Удаляет сборку абстракций"
    This reverts commit 4a35df2a3fda0ca40e35bd79872cbc42aaf2f293.

--- EFCore.TemporaryTables.Abstractions/ITemporaryTableOperation.cs
+++ EFCore.TemporaryTables.Abstractions/ITemporaryTableOperation.cs
@@ -3,6 +3,6 @@ namespace EFCore.TemporaryTables.Abstractions;
 public interface ITemporaryTableOperation
 {
     Task ExecuteAsync<TEntity>(CancellationToken cancellationToken = default) where TEntity : class;
-
+    
     void Execute<TEntity>() where TEntity : class;
 }
